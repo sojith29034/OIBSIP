2c2de386133dca902d1bda689aa5be723a1ae605
Sojith Sunny <sojithsunny29034@gmail.com>
2023-11-18 01:56:47 +0530

    3rd day

--- temp-converter/js.js
+++ temp-converter/js.js
@@ -29,40 +29,40 @@ function convertTemp(){
     if(in_temp.value === "Degree Celsius"){
         if(out_temp.value === "Degree Fahrenheit"){
             let output = (((inVal * 9)/5) + 32);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " &deg;F";
         }
         else if(out_temp.value === "Kelvin"){
             let output = ((inVal * 1) + 273.15);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " K";
         }
         else{
-            result.innerHTML = inVal;
+            result.innerHTML = inVal + " &deg;C";
         }
     }
     else if(in_temp.value === "Degree Fahrenheit"){
         if(out_temp.value === "Degree Celsius"){
             let output = (((inVal * 1)-32) * 5/9);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " &deg;C";
         }
         else if(out_temp.value === "Kelvin"){
             let output = ((((inVal * 1)-32) * 5/9) + 273.15);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " K";
         }
         else{
-            result.innerHTML = inVal;
+            result.innerHTML = inVal + " &deg;F";
         }
     }
     else if(in_temp.value === "Kelvin"){
         if(out_temp.value === "Degree Celsius"){
             let output = ((inVal * 1)-273.15);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " &deg;C";
         }
         else if(out_temp.value === "Degree Fahrenheit"){
             let output = ((((inVal * 1) - 273.15) * 9/5) + 32);
-            result.innerHTML = output.toFixed(2);
+            result.innerHTML = output.toFixed(2) + " &deg;F";
         }
         else{
-            result.innerHTML = inVal;
+            result.innerHTML = inVal + " K";
         }
     }
 }
